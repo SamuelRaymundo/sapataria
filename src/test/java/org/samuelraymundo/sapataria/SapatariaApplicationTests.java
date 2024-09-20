23e214138d25a658aca607bd4966ae096ff08c07
Samuel Raymundo <161658735+SamuelRaymundo@users.noreply.github.com>
2024-09-20 05:11:15 -0300

    starting project

---
+++ src/test/java/org/samuelraymundo/sapataria/SapatariaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samuelraymundo.sapataria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapatariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
